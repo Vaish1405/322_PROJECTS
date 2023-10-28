b8bc82f969746421867f3720a7619a803f40a3a3
Vaishnavi Sen <vaishnavisen@Vaishnavis-MBP.lan>
2023-10-28 10:58:27 -0700

    base functions added

---
+++ project7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+
+void functionToRunByThreads() {
+
+}
+
+void P(Semaphore s) {
+
+}
+
+void V(Semaphore s) {
+
+}
+
+void block(p) {
+
+}
+
+void wakeup(p) {
+    
+}
+
+
+int main() {
+
+    return 0; 
+}
